1bcfdfb607ef8a9a991fe7065b88b8e66a4f1cb9
kersaspcawasji <kersasp94@gmail.com>
2017-09-28 17:46:43 -0400

    added block for different nodes and made sure all rhos are stable

--- MATLAB/master_code.m
+++ MATLAB/master_code.m
@@ -8,45 +8,51 @@ clear
 
 % Delivery Node
 
-lambda_D = 100; 
+lambda_D = 200; 
 
 mean_load_DS = 10;      % 10 units
 agv_speed = 5.4;        % based on Otto (km/hr)
 n_DS = 1;               % No. of AGVs 
 d_DS = 0.040;           % distance between D and S = 40m = 0.04km
 
-mu_DS = mean_load_DS*agv_speed*n_DS/d_DS
+mu_DS = mean_load_DS*n_DS/((2*d_DS/agv_speed)+(2/60))
 
 rho_DS = lambda_D/mu_DS
 
 % Storage Node
 
-lambda_S = mu_DS; 
+lambda_S = mu_DS 
 
 mean_load_SM = 10;      % 10 units
 agv_speed = 5.4;        % based on Otto (km/hr)
 n_SM = 1;               % No. of AGVs 
 d_SM = 0.030;           % distance between D and S = 30m = 0.03km
 
-mu_SM = mean_load_SM*agv_speed*n_SM/d_SM
+mu_SM = mean_load_SM*n_SM/((2*d_SM/agv_speed)+2/60)
 
 rho_SM = lambda_S/mu_SM
 
 % Manufacturing Node
 
-lambda_M = mu_SM; 
-mu_M = 120;             % Assumed Manufacturing rate = 12 units/hr
+lambda_M = mu_SM 
+mu_M = 300;             % Assumed Manufacturing rate = 1000 units/hr
 
 rho_M = lambda_M/mu_M
+
+W_M = rho_M/(mu_M-lambda_M)*60  % Wait time (hours/unit)
 
 % Manufacturing Transportation Node
 
-lambda_M = mu_SM; 
-mu_M = 120;             % Assumed Manufacturing rate = 12 units/hr
+lambda_MB = mu_M 
+mean_load_MB = 10;      % 10 units
+agv_speed = 5.4;        % based on Otto (km/hr)
+n_MB = 1;               % No. of AGVs 
+d_MB = 0.030;           % distance between D and S = 200m = 0.200km
 
-rho_M = lambda_M/mu_M
+mu_MB = mean_load_MB*n_MB/((2*d_MB/agv_speed)+2/60)
 
-% Testing git
+rho_MB = lambda_MB/mu_MB
+
 
 
 
