96fcc4c0339f05c88511ec2754e37d7aeab251cf
samvrit <samvrit.goku@gmail.com>
2017-11-18 16:58:59 -0500

    trade-off analysis added

--- MATLAB/tradeoff_analysis.m
+++ MATLAB/tradeoff_analysis.m
@@ -1,9 +1,12 @@
-filename = 'results_sensitivity.xlsx';
-A = xlsread(filename,'Test Cases');
+filename = 'results_tradeoff.xlsx';
+A = xlsread(filename,'Inputs');
 n = size(A,1);
 idle_time = zeros(1,n);
 lead_time = zeros(1,n);
 data_tuple = zeros(n,18);
+t = 10000;
+nn = 5;
+[~, analysis_type] = xlsread(filename,'Alternatives','B20');
 for i = 1:n
     agv_speed = A(i,1);
     agv_mean_load = A(i,6:9);
@@ -12,9 +15,17 @@ for i = 1:n
     node_distances = A(i,10:13);
     mfg_rate = A(i,15);
     pkg_rate = A(i,16);
-    [~, lead_time(i), idle_time(i)] = agv_plant(agv_speed, ...
+    if strcmp(analysis_type{1},'Analytical')
+        [~, lead_time(i), idle_time(i)] = agv_plant(agv_speed, ...
           agv_mean_load, agv_count, arrival_rate, node_distances, ...
           mfg_rate, pkg_rate);
+    elseif strcmp(analysis_type{1},'Monte Carlo')
+        [lead_time(i), ~, idle_time(i), ~, ~, ~, ~, ~] = montecarlo(agv_speed, ...
+          agv_mean_load, agv_count, arrival_rate, node_distances, ...
+          mfg_rate, pkg_rate, t, nn);
+    else
+        disp('Select analysis type in Excel file.');
+    end
     fprintf('Row %d \n',i);
     data_tuple(i,:) = [agv_speed, agv_count, agv_mean_load, node_distances, arrival_rate, mfg_rate, pkg_rate, lead_time(i), idle_time(i)];
 end
